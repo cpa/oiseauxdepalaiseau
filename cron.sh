066fa5d4f8879ca3295307bbd6f8a8ba7a645748
rpi <rpi@local>
2026-02-14 09:10:07 +0100

    Updated data

--- cron.sh
+++ cron.sh
@@ -1,5 +1,7 @@
-cp ~/BirdNET-Pi/BirdDB.txt .
-./birddb_to_json.py > ~/oiseauxdepalaiseau/birddb.json
+cd /home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/
+/home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/
+cp /home/birdlistener/BirdNET-Pi/BirdDB.txt /home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/
+/home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/birddb_to_json.py > /home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/birddb.json
 git add .
 git commit -a -m "Updated data"
 git pull origin main
