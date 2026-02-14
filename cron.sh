b2b239c68d0dba71050ebdf25e076c73e2e69729
rpi <rpi@local>
2026-02-14 08:45:52 +0100

    added cron

--- cron.sh
+++ cron.sh
@@ -1,5 +1,5 @@
 cp ~/BirdNET-Pi/BirdDB.txt .
-./birddb_to_json.py > birddb.json
+./birddb_to_json.py > ~/oiseauxdepalaiseau/birddb.json
 git add .
 git commit -a -m "Updated data"
 git pull origin main
