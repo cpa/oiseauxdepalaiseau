27da8b7b9ebb9acc49fe7166141a1131429b5dff
rpi <rpi@local>
2026-02-14 09:45:07 +0100

    Updated data

--- cron.sh
+++ cron.sh
@@ -1,5 +1,4 @@
 cd /home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/
-/home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/
 cp /home/birdlistener/BirdNET-Pi/BirdDB.txt /home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/
 /home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/birddb_to_json.py > /home/birdlistener/BirdNET-Pi/oiseauxdepalaiseau/birddb.json
 git add .
